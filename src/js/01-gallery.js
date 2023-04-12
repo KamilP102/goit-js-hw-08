d513ac1bc19fd17505370cd202597e978ff426e4
KamilP102 <kamilp1319@gmail.com>
2023-04-12 18:15:13 +0200

    Update on 01 and 02 HW

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const imageList = galleryItems
+  .map(
+    item =>
+      `<a class="gallery__item" href=${item.original}>
+    <img
+      class="gallery__image"
+      src=${item.preview}
+      alt=${item.description}
+    />
+  </a>`
+  )
+  .join('');
+gallery.insertAdjacentHTML('afterbegin', imageList);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  sourceAttr: `href`,
+  captionsData: `alt`,
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import _ from 'lodash';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const saveUpdate = ({ seconds }) => {
+  localStorage.setItem('videoplayer-current-time', seconds);
+};
+const getSavedTime = () => {
+  return localStorage.getItem('videoplayer-current-time');
+};
+
+player
+  .setCurrentTime(getSavedTime())
+  .then(function (data) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
+
+player.on('timeupdate', _.throttle(saveUpdate, 1000));
