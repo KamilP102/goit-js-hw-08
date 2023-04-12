0bac71d0dac565628c5b4991422e9c56be0d2754
KamilP102 <kamilp1319@gmail.com>
2023-04-12 18:53:45 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
+import _ from 'lodash';
 
+const emailEl = document.querySelector('input[type=email');
+const messageEl = document.querySelector('textarea[name=message]');
+const btnEl = document.querySelector('button[type=submit');
+const formEl = document.querySelector('.feedback-form');
+
+const data = {
+  email: '',
+  message: '',
+};
+getData = () => {
+  data.email = emailEl.value;
+  data.message = messageEl.value;
+  localStorage.setItem(STORAGEKEY, JSON.stringify(data));
+};
+submitForm = () => {
+  localStorage.clear();
+  formEl.reset();
+  console.log(STORAGEKEY);
+};
+
+
+const STORAGEKEY = 'feedback-form-state';
+addEventListener.formEl('input', _.throttle(getData), 500);
+addEventListener.btnEl('click', submitForm);
