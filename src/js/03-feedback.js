90bef784990dcadef31649964ad828411c2c70b0
KamilP102 <kamilp1319@gmail.com>
2023-04-14 17:01:56 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,32 +5,31 @@ const messageEl = document.querySelector('textarea[name=message]');
 const btnEl = document.querySelector('button[type=submit');
 const formEl = document.querySelector('.feedback-form');
 const STORAGEKEY = 'feedback-form-state';
-updateOutput();
+
+const updateOutput = () => {
+  const data = JSON.parse(localStorage.getItem(STORAGEKEY)) || '';
+  emailEl.value = data.email || '';
+  messageEl.textContent = data.message;
+};
 
 const addData = {
   email: '',
   message: '',
 };
-
-saveData = _.throttle(() => {
+const saveData = () => {
   addData.email = emailEl.value;
   addData.message = messageEl.value;
   localStorage.setItem(STORAGEKEY, JSON.stringify(addData));
-}, 500);
+  return;
+};
 
-submitForm = e => {
+const submitForm = e => {
   e.preventDefault();
+  console.log(addData);
   localStorage.clear();
   formEl.reset();
   messageEl.innerHTML = '';
 };
 
-formEl.addEventListener('input', saveData);
+formEl.addEventListener('input', _.throttle(saveData, 500));
 btnEl.addEventListener('click', submitForm);
-
-function updateOutput() {
-  const data = JSON.parse(localStorage.getItem(STORAGEKEY)) || '';
-  emailEl.value = data.email || '';
-  messageEl.textContent = data.message;
-  console.log(data);
-}
